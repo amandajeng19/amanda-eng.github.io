95588f9fa1539f405b8544c1d4bc0a9821014765
Amanda Eng <amandajeng19@gmail.com>
2026-01-31 06:39:41 -0800

    stlyed the accordion

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-var acc = document.getElementsByClassName("accordion");
+// Learned to create the accordion from here: https://www.w3schools.com/howto/howto_js_accordion.asp
+
+var accord = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
+for (i = 0; i < accord.length; i++) {
+  accord[i].addEventListener("click", function () {
     this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
