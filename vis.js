200a20d576b9ba019f475e6ee2cc678049feb6f2
Amanda Eng <amandajeng19@gmail.com>
2026-01-31 05:05:59 -0800

    stars are spinning

---
+++ vis.js
@@ -0,0 +1,10 @@
+let svg = document.querySelector("svg");
+
+function spinStar (starID) {
+    document.getElementById(starID);
+    
+}
+
+function changeRadius() {
+  document.getElementById("circle1").setAttribute("r", "50");
+}
