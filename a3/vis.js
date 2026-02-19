f4a05f3e02b4be2af1253eba91799544d60a0873
Amanda Eng <amandajeng19@gmail.com>
2026-02-19 13:30:42 -0800

    added remaining vis for part 2

--- a3/vis.js
+++ a3/vis.js
@@ -3,15 +3,18 @@
 // Load data from datasets/videogames_wide.csv using d3.csv and then make visualizations
 async function fetchData() {
   console.log("called fetchData");
-  const data = await d3.csv("dataset/videogames_wide.csv");
+  const gamesData = await d3.csv("dataset/videogames_wide.csv");
   console.log("loaded dataset");
-  return data;
+  return gamesData;
 }
 
-fetchData().then(async (data) => {
+fetchData().then(async (gamesData) => {
+  // 2.1 Global Sales by Genre and Platform
+
+  // Question 2.1 A: Which video game genres had the highest sales globally?
   const vlSpec = vl
     .markBar()
-    .data(data)
+    .data(gamesData)
     .encode(
       vl
         .y()
@@ -21,24 +24,100 @@ fetchData().then(async (data) => {
       vl.x().fieldN("Genre").sort("-y"),
       vl.color().fieldN("Platform"),
     )
-    .width(400)
+    .width(800)
     .height(400)
     .toSpec();
 
+  // Question 2.1 B: Which gaming platforms had the highest sales globally?
   const vlSpec2 = vl
     .markBar()
-    .data(data)
+    .data(gamesData)
     .encode(
-      vl.y().fieldN("Platform").sort("-x"),
-      vl.x().fieldQ("Global_Sales").aggregate("sum"),
-      vl.color().value("teal"),
+      vl
+        .y()
+        .fieldQ("Global_Sales")
+        .aggregate("sum")
+        .title("Total Global Sales (in Millions)"),
+      vl.x().fieldN("Platform").sort("-y"),
+      vl.color().fieldN("Genre"),
     )
-    .width(400)
+    .width(800)
     .height(400)
+    .toSpec();
+
+  // 2.1 Sales Over Time by Platform and Genre
+
+  // Question 2.2 A: What were the peak sale years for the top-selling gaming platforms?
+  const vlSpec3 = vl
+    .markLine()
+    .data(gamesData)
+    .transform(vl.filter('datum["Year"] != "N/A"'))
+    .encode(
+      vl
+        .y()
+        .fieldQ("Global_Sales")
+        .aggregate("sum")
+        .title("Total Global Sales (in Millions)"),
+      vl.x().fieldO("Year").title("Year Released"),
+      vl.color().fieldN("Platform"),
+      vl.tooltip([
+        { field: "Year", type: "quantitative", title: "Year" },
+        { field: "Platform", type: "nominal" },
+      ]),
+    )
+    .width(800)
+    .height(400)
+    .toSpec();
+
+  // Question 2.2 B: Which gaming platforms had the highest sales in North America?
+  const vlSpec4 = vl
+    .markLine()
+    .data(gamesData)
+    .transform(vl.filter('datum["Year"] != "N/A"'))
+    .encode(
+      vl
+        .y()
+        .fieldQ("Global_Sales")
+        .aggregate("sum")
+        .title("Total Global Sales (in Millions)"),
+      vl.x().fieldO("Year").title("Year Released"),
+      vl.color().fieldN("Genre"),
+      vl.tooltip([
+        { field: "Year", type: "quantitative", title: "Year" },
+        { field: "Genre", type: "nominal" },
+      ]),
+    )
+    .height(400)
+    .toSpec();
+
+  // Question 2.3 A: Which platforms sold the best in each region?
+  const vlSpec5 = vl
+    .markBar()
+    .data(gamesData)
+    .transform({
+      fold: ["NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales"],
+      as: ["key", "value"],
+    })
+    .encode(
+      vl.column({ field: "key", type: "nominal" }).title("Region"),
+      vl.y().fieldQ("value").aggregate("sum").title("Game sales (Millions)"),
+      vl.x().fieldN("Platform"),
+      vl.color().fieldN("Platform"),
+      vl.tooltip([
+        { field: "key", type: "nominal", title: "Region" },
+        { field: "Platform", type: "nominal" },
+        { field: "value", type: "quantitative", title: "Sales (Million)" },
+      ]),
+    )
+    .width(250)
+    .height(300)
     .toSpec();
 
   render("#view2-1-A", vlSpec);
   render("#view2-1-B", vlSpec2);
+  render("#view2-2-A", vlSpec3);
+  render("#view2-2-B", vlSpec4);
+  render("#view2-3-A", vlSpec5);
 });
 
 async function render(viewID, spec) {
