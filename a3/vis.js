9bb52f31be030004f8c219c1161fb57598648748
Amanda Eng <amandajeng19@gmail.com>
2026-02-18 23:58:47 -0800

    got first vis in place

--- a3/vis.js
+++ a3/vis.js
@@ -1,21 +1,27 @@
-import d3 from "d3";
+// import { vl } from "@vega/vega-lite-api-v5";
 
 // Load data from datasets/videogames_wide.csv using d3.csv and then make visualizations
 async function fetchData() {
-  const data = await d3.csv("./dataset/videogames_wide.csv");
+  console.log("called fetchData");
+  const data = await d3.csv("dataset/videogames_wide.csv");
+  console.log("loaded dataset");
   return data;
 }
-console.log(data);
 
 fetchData().then(async (data) => {
   const vlSpec = vl
     .markBar()
     .data(data)
     .encode(
-      vl.y().fieldN("Platform").sort("-x"),
-      vl.x().fieldQ("Global_Sales").aggregate("sum"),
+      vl
+        .y()
+        .fieldQ("Global_Sales")
+        .aggregate("sum")
+        .title("Total Global Sales (in Millions)"),
+      vl.x().fieldN("Genre").sort("-y"),
+      vl.color().fieldN("Platform"),
     )
-    .width("container")
+    .width(400)
     .height(400)
     .toSpec();
 
@@ -23,16 +29,16 @@ fetchData().then(async (data) => {
     .markBar()
     .data(data)
     .encode(
-      vl.y().fieldN("Genre").sort("-x"),
+      vl.y().fieldN("Platform").sort("-x"),
       vl.x().fieldQ("Global_Sales").aggregate("sum"),
       vl.color().value("teal"),
     )
-    .width("container")
+    .width(400)
     .height(400)
     .toSpec();
 
-  render("#view", vlSpec);
-  render("#view2", vlSpec2);
+  render("#view2-1-A", vlSpec);
+  render("#view2-1-B", vlSpec2);
 });
 
 async function render(viewID, spec) {
